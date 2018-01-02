ef8e67485f044657e3fa7f2e8dcb34a272c8aa70
Emanuel <ENunezSardinha@Gmail.com>
2018-01-02 17:41:20 +0100

    Figured out how to draw stuff TODO: Stack structures

--- Stigweb/script.js
+++ Stigweb/script.js
@@ -24,6 +24,9 @@ function runCode() {
     alert("hello world");
 
     document.getElementById("console").innerHTML += 5 + 6;
+
+    
+
     /*
     var stack = new Stack();
 
@@ -55,4 +58,21 @@ function runCode() {
         }
     
         */
+}
+
+
+
+function myMove() {
+    var elem = document.getElementById("myAnimation"); 
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++; 
+            elem.style.top = pos + 'px'; 
+            elem.style.left = pos + 'px'; 
+        }
+    }
 }
