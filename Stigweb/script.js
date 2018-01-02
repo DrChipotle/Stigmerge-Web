b547efb8eafc5920994b6875a1794e5d64933910
Emanuel <ENunezSardinha@Gmail.com>
2018-01-02 17:31:46 +0100

    Trying the algorithm from the web browser! Currently figuring out how this Javascript thing works

---
+++ Stigweb/script.js
@@ -0,0 +1,58 @@
+function runCode() {
+
+
+    /*
+    function Stack(i_lim) {
+    
+        i_limit;
+
+        this.stac = new Array();
+
+        this.pop = function () {
+            return this.stac.pop();
+        }
+
+        this.push = function (item) {
+            this.stac.push(item);
+        }
+    }
+    */
+    
+
+
+    
+    alert("hello world");
+
+    document.getElementById("console").innerHTML += 5 + 6;
+    /*
+    var stack = new Stack();
+
+    
+    stack.push("A");
+    stack.push("B");
+    stack.push("C");
+
+    alert(stack.pop());
+    alert(stack.pop());
+    alert(stack.pop());
+
+*/
+    /*
+    
+        function Stack(i_lim) {
+    
+            i_limit;
+    
+            this.stac = new Array();
+    
+            this.pop = function () {
+                return this.stac.pop();
+            }
+    
+            this.push = function (item) {
+                this.stac.push(item);
+            }
+        }
+    
+        */
+}
