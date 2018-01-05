887987cd5180af74e6dc77a06b8a42647ea41971
Emanuel <ENunezSardinha@Gmail.com>
2018-01-05 12:55:38 +0100

    Testing hot to use chart.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function limStack(i_lim) {
         return this.limit;
     }
 
+    
     /*
 
 
@@ -98,3 +99,25 @@ function myMove() {
         }
     }
 }
+
+
+new Chart(document.getElementById("bar-chart"), {
+    type: 'bar',
+    data: {
+      labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+      datasets: [
+        {
+          label: "Population (millions)",
+          backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+          data: [2478,5267,734,784,433]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+});
